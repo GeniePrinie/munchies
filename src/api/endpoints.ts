e7d7883ca651bf95d773119c7d228198238579e3
genieprinie <geniexpn@gmail.com>
2024-11-20 10:20:46 +0100

    update base url

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  "https://work-test-web-2024-eze6j4scpq-lz.a.run.app/api/";
+  "https://work-test-web-2024-eze6j4scpq-lz.a.run.app/api";
 
 export const ENDPOINTS = {
   restaurants: `${API_BASE_URL}/restaurants`,
